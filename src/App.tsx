eca4c3387bf27d96a283f0f304c7ad9c60c98bb3
Mohaimen Al Rashid <kdmars0168@gmail.com>
2025-02-16 14:41:38 +0800

    Removed animated background for better performance

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
-import { AnimatedBackground } from './components/AnimatedBackground';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-    <AnimatedBackground />
       <Toaster />
       <Sonner />
       <BrowserRouter>
